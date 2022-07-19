9bdd8a12c5a01e6abe21a5dd305659f5bb31a8a4
jgoodson <jgoodson@perview365.onmicrosoft.com>
2022-07-19 18:48:45 -0500

    more working

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,31 +4,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'collapsible-well',
   templateUrl: './collapsible-well.component.html',
-  styles: [`
-  .well.pointable {
-    margin-bottom: 0;
-    margin-top: 0;
-    padding-left: 0
-    
-}
-
-.well.pointable {
-    padding-bottom: .1em;
-    padding-top: auto;
-  
-}
-
-div {
-  margin: 0;
-  
-}
-.datesForPrint {
-  display: none;
- 
-}
-
-  `]
+  styleUrls: ['./collapsible-well.component.css']
 })
+
 export class CollapsibleWellComponent {
   visible: boolean = true;
 
